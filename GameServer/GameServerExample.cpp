2f1d8242a64fefb3a99a90df54c99f35546a16ca
lsmg <rjd67441@hotmail.com>
2022-05-28 00:30:18 +0800

    增加简易的回放功能，未同步帧号

--- GameServer/GameServerExample.cpp
+++ GameServer/GameServerExample.cpp
@@ -2,6 +2,8 @@
 #include "Message/Messages.h"
 #include "Utils/TimeUtils.h"
 
+
+
 void GameServerExample::Ping(ROLE_ID role_id, const BaseMessagePtr& msg)
 {
 	PingMessagePtr ping_ptr = CastBaseMsgTo<PingMessage>(msg);
@@ -18,10 +20,21 @@ void GameServerExample::Control(ROLE_ID role_id, const BaseMessagePtr& msg)
 			role_ids.push_back(role_id);
 		}
 		std::cout << fmt::format("add a new role: {}\r\n", role_id);
+
+		if (role_id == 2)
+		{
+			frame_counter_ = 0;
+		}
+		if (role_id == 3)
+		{
+			RePlay(role_id);
+		}
 	}
 	else
 	{
-		SendMessageByRoleIds(role_ids, msg);
+		// SendMessageByRoleIds(role_ids, msg);
+		std::lock_guard<std::mutex> guard(control_msg_lock_);
+		control_msg_.push_back(msg);
 	}
 }
 
@@ -50,7 +63,10 @@ void GameServerExample::OnRoleDisconnect(int32_t role_id)
 
 
 GameServerExample::GameServerExample(const std::string& name, unsigned short port):
-	MKServer(name, port, 10, 10)
+	MKServer(name, port, 10, 10),
+	running_(true),
+	frame_counter_(true),
+	frame_input_record_vec_()
 {
 	SET_MSG_FUNC(MessageType::PING, Ping);
 	SET_MSG_FUNC(MessageType::CONTROL, Control);
@@ -60,4 +76,58 @@ GameServerExample::GameServerExample(const std::string& name, unsigned short por
 	{
 		OnRoleDisconnect(role_id);
 	});
+
+	last_frame_begin_time = std::chrono::steady_clock::now();
+	main_thread_ = std::thread(
+			[this]()
+			{
+				while (running_)
+				{
+					auto frame_begin_time = std::chrono::steady_clock::now();
+					auto delta_time = frame_begin_time - last_frame_begin_time;
+					last_frame_begin_time = frame_begin_time;
+
+					Update(std::chrono::duration_cast<std::chrono::milliseconds>(delta_time));
+
+					auto frame_end_time = std::chrono::steady_clock::now();
+
+					auto frame_cost = frame_end_time - frame_begin_time;
+					std::this_thread::sleep_for(LOGIC_FRAME_MS - frame_cost);
+				}
+			});
+}
+
+GameServerExample::~GameServerExample()
+{
+	running_ = false;
+	main_thread_.join();
+}
+
+void GameServerExample::Update(std::chrono::milliseconds delta_time)
+{
+	frame_counter_++;
+	{
+		std::lock_guard guard(control_msg_lock_);
+		send_msg_.swap(control_msg_);
+	}
+	frame_input_record_vec_.push_back({frame_counter_, send_msg_});
+
+	for (auto& msg : send_msg_)
+	{
+		auto cont_msg_ptr = CastBaseMsgTo<ControlMessage>(msg);
+		cont_msg_ptr->tick = frame_counter_;
+		SendMessageByRoleIds(role_ids, cont_msg_ptr);
+	}
+	send_msg_.clear();
+}
+
+void GameServerExample::RePlay(ROLE_ID role_id)
+{
+	for (FrameInput& frame_input : frame_input_record_vec_)
+	{
+		for (auto& msg : frame_input.send_msg)
+		{
+			SendMessageByRoleId(role_id, msg);
+		}
+	}
 }
--- GameServer/GameServerExample.h
+++ GameServer/GameServerExample.h
@@ -6,6 +6,7 @@ class GameServerExample : public MKServer
 {
 public:
 	GameServerExample(const std::string& name, unsigned short port);
+	~GameServerExample();
 
 	void Ping(ROLE_ID role_id, const BaseMessagePtr& msg);
 	void Control(ROLE_ID role_id, const BaseMessagePtr& msg);
@@ -15,4 +16,29 @@ private:
 
 	std::mutex role_ids_mutex_;
 	std::vector<ROLE_ID> role_ids;
+
+private:
+
+	bool running_;
+	std::thread main_thread_;
+
+	const int LOGIC_FPS = 10;
+	const std::chrono::milliseconds LOGIC_FRAME_MS{1000 / LOGIC_FPS};
+	std::chrono::time_point<std::chrono::steady_clock> last_frame_begin_time;
+
+	std::vector<BaseMessagePtr> control_msg_;
+	std::vector<BaseMessagePtr> send_msg_;
+	std::mutex control_msg_lock_;
+	uint32_t frame_counter_;
+
+	struct FrameInput
+	{
+		uint32_t frame_number;
+		std::vector<BaseMessagePtr> send_msg;
+	};
+	std::vector<FrameInput> frame_input_record_vec_;
+
+	void Update(std::chrono::milliseconds delta_time);
+
+	void RePlay(ROLE_ID role_id);
 };
